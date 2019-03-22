82ae9ef6e0f23e6c83c1e128125470145bbdec9a
suwy <1573243972@qq.com>
2019-03-22 08:53:35 +0800

    初始化

---
+++ src/main/java/com/work/rabbitmq/Receiver1.java
@@ -0,0 +1,15 @@
+package com.work.rabbitmq;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RabbitListener(queues = "hello")
+public class Receiver1 {
+    @RabbitHandler
+    public void process(String msg){
+        System.out.println("Receiver收到了消息:"+msg);
+    }
+
+}
